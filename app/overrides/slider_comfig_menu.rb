daddd78fa02474de45e7211087c597a66e9c8c26
Volodia Manivchuk <manivchuk.volodia@gmail.com>
2015-04-05 14:07:35 +0300

    Add slides to configuration menu

---
+++ app/overrides/slider_comfig_menu.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  virtual_path: 'spree/layouts/admin',
+  name: 'slider_admin_sidebar_menu',
+  insert_bottom: '#sidebar-configuration',
+  erb: q{<li><%= link_to t('spree_slider.config_name'), admin_slides_path %></li>}
+)
